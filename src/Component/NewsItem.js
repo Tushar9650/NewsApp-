97aa4df4744d4cdac050348e04170bf80bba895d
tushar <tusharsinghota@gmail.com>
2023-03-26 00:59:00 +0530

    deploy

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <p className="card-text"><small class="text-muted">By {author?author:"Unknown"} on {date}</small></p>
+                        <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {date}</small></p>
                         <h6><span className="badge bg-secondary">{source}</span></h6>
                         <a href={newsUrl} className="btn btn-sm btn-dark"  target="_blank">Go somewhere</a>
                     </div>
